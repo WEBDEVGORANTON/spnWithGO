5185b45ef10afa7e02b4ebfa115356038d0c7ea2
anton <goranton@octopod.com>
2018-05-26 12:04:53 +0300

    cert file locations init

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'certs' => [
+            'driver' => 'local',
+            'root' => storage_path('app/certs')
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
